8e0973f7b469ab1ec5aa4cb64c2f1be8783657b1
ObsidianAsphodel <terencep.dixon@gmail.com>
2023-08-17 22:54:51 -0700

    Finished "Radians To Degrees"

---
+++ Radians To Degrees/ConvertRadiansIntoDegrees.js
@@ -0,0 +1,8 @@
+radiansIntoDegrees = (radians) => {
+    let degrees = radians * (180 / Math.PI)
+    console.log("Number of degrees:" + Math.round(degrees));
+}
+radiansIntoDegrees(4)
+radiansIntoDegrees(2)
+radiansIntoDegrees(5)
+// Turns radians into degrees
